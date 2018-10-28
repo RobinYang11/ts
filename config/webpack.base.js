741bd2416d86d63b5c232b40d039780d0f6706f1
robinyang11 <756517553@qq.com>
2018-10-28 19:46:18 +0800

    init project

---
+++ config/webpack.base.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [{
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: ['img:src']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name]-aaa.[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'dev'
+        }),
+        new ExtractTextPlugin("styles.css"),
+    ],
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,40 @@
+
+
+const port =8089
+const merge = require('webpack-merge');
+const common = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+
+module.exports = merge(common, {
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: '../dist', 
+        hot: true,
+        port:port
+    },
+    plugins: [
+        
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(['dist']),        
+        new HtmlWebpackPlugin({                 
+            title: 'Output Management',
+            template: "./src/index.html" 
+        }),
+        new FriendlyErrorsWebpackPlugin({ 
+            compilationSuccessInfo: {  
+                messages: [`You application is running here http://localhost:${port}`],
+                notes: ['Some additionnal notes to be displayed unpon successful compilation']
+            },
+            onErrors: function (severity, errors) {
+                
+            },
+            
+            clearConsole: true,
+        }),
+    ]
+});
